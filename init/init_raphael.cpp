79976ac94b5f5492de65e4b7100ce09b9a94500c
carpe462 <carpe462@gmail.com>
2020-08-31 06:01:59 -0700

    raphael: update ro.build.description as per miui 12

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -89,7 +89,7 @@ void vendor_load_properties()
     }
 
     // fingerprint
-    property_override("ro.build.description", "raphael-user 10 QKQ1.190825.002 V11.0.8.0 release-keys");
+    property_override("ro.build.description", "raphael-user 10 QKQ1.190825.002 V12.0.2.0.QFKCNXM release-keys");
     property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys");
 
     load_dalvikvm_properties();

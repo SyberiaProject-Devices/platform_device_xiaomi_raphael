0219e58b1ac2034ec2c45ce3ce97162dfe86156e
Albert Wang <albertccwang@google.com>
2021-03-25 00:03:02 +0800

    raphael: Update USB HAL to V1.3 implementation
    Supports to enable/disable USB data signaling
    
    Bug: 161414036
    Test: Pass USB V1.3 HIDL tests
    Signed-off-by: Albert Wang <albertccwang@google.com>
    Change-Id: Iffe00f8753206fb66cd3ab96cae5aa5ad9c410cd

--- device.mk
+++ device.mk
@@ -472,7 +472,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.3-service-qti
 
 # Vendor service manager
 PRODUCT_PACKAGES += \

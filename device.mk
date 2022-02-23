19bdb6b747e25056b354248016362558d4a35abf
kondors1995 <normandija1945@gmail.com>
2022-02-23 14:24:44 +0000

    raphael: Import WiFi Display from LA.UM.9.1.r1-11500.02-SMxxx0.QSSI12.0

--- device.mk
+++ device.mk
@@ -490,6 +490,21 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
+
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libdisplayconfig.qti \
+    libdisplayconfig.system.qti \
+    libnl \
+    libqdMetaData \
+    libqdMetaData.system \
+    libwfdaac_vendor \
+    WfdCommon
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
 # Updater
 PRODUCT_PACKAGES += \
     Updater
+

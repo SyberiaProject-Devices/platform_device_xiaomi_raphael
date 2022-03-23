659e618b2f8573150fe49d5fbe1492d93f703fb2
kondors1995 <normandija1945@gmail.com>
2022-03-23 13:53:52 +0000

    raphael: Build more camera libs

--- device.mk
+++ device.mk
@@ -153,9 +153,10 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libdng_sdk.vendor \
     libgui_vendor \
-    Snap \
+    libstdc++.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
-    vendor.xiaomi.hardware.motor@1.0.vendor
+    vendor.xiaomi.hardware.motor@1.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor
 
 PRODUCT_PACKAGES += \
     GoogleCamera
